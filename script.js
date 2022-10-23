e115b24ab328eb9e809f4d432b7c6080e03ad8df
teuprimo <vinicius.brandaot@gmail.com>
2022-10-22 23:21:29 -0300

    comparacao de cartas

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
 let cartas = prompt('Quantas cartas você quer? \n Digite um número par de 4 a 14.');
 
-let jogo = false;
+let jogo = false; // diz se o jogo iniciou
+
+let contador=0; //conta o numero de jogadas
+
+let cartaAnterior,  indexCartaAnterior; // carta escolhida na rodada anterior
 
 while(jogo!=true){
     if (((cartas%2)!=0)||(cartas<4)||(cartas>14)){
@@ -31,22 +35,22 @@ function criarCards(cartas){
 
     const card = document.querySelector('.cards');
     console.log(card);
-    let par, aux;
+    let par, aux, i=0;
     let alreadyIn = [];
 
     
 
     while( aux != true){
-        //Criando os cards//  
+        //Criando os cards já embaralhadas//  
         par = parseInt(Math.random() *cartas);
-       
+    
         
         if(alreadyIn.includes(par)===false){
             if(par%2!=0){
                 alreadyIn.push(par);
-                par = par - 1;
-                card.innerHTML = card.innerHTML + `<li class = "card" id="pair${par}"> 
-                <img src="./Archives/back.png" id="parrot" alt="">
+                par--;
+                card.innerHTML = card.innerHTML + `<li class = "card" id="pair${par}" onclick="select(this, ${par}, ${i})"> 
+                <img src="./Archives/back.png" id="parrot" alt="">${par}
                 </li>
                 
                 `;
@@ -54,8 +58,8 @@ function criarCards(cartas){
                 console.log(`carta de par ${par}`);
             }
             else{
-                card.innerHTML = card.innerHTML + `<li class = "card" id="pair${par}"> 
-                <img src="./Archives/back.png" id="parrot" alt="">
+                card.innerHTML = card.innerHTML + `<li class = "card" id="pair${par}" onclick="select(this, ${par}, ${i})"> 
+                <img src="./Archives/back.png" id="parrot" alt="">${par}
                 </li>
                 
                 `;
@@ -64,7 +68,7 @@ function criarCards(cartas){
                 alreadyIn.push(par);
             }
            
-
+            i++;
            
             console.log(alreadyIn);
         }
@@ -73,13 +77,30 @@ function criarCards(cartas){
         }
         else{}
       
-        console.log(`carta ${par} criada`);
-
+        console.log(`carta ${par} criada\nindice ${i}`);
+        
     }
-    //Embaralhando cartas//
-
-
 
 }
 
+//selecao de cartas//
 
+function select(selected, id, cardIndex){
+    if(cardIndex === indexCartaAnterior){
+        alert('Selecione uma carta diferente');
+        return 0;
+    }
+
+    contador++;
+    console.log("carta selecionada " );
+    selected.classList.toggle('selected');
+    let cartaEscolhida = id;
+    
+    if (cartaEscolhida===cartaAnterior){
+        console.log("igual");
+    }
+
+    
+    cartaAnterior = id;
+    indexCartaAnterior = cardIndex;
+}
